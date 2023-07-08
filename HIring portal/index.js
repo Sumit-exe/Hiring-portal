a9816c78a541a10e0d57147b03d7d576e498b3bf
Sumit-exe <97934852+Sumit-exe@users.noreply.github.com>
2023-07-09 00:26:50 +0530

    Add files via upload

---
+++ HIring portal/index.js
@@ -0,0 +1,10 @@
+// let cards = document.querySelectorAll(".sideBarCard");
+
+// // card.onclick = () =>{
+// //     card.classList.add('.active');
+// // }
+// cards.forEach(card =>{
+    
+//     card.classList.add('active')
+//     // document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+//   });
